3085a9e8ad1d290a8e4210afb390a98a16b26649
Katya-Rotar <katya.rotar02@gmail.com>
2025-05-13 16:12:00 +0300

    feat: implemented service, dtos, mapping for Clothing Item

---
+++ BLL/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using BLL.DTO;
+using BLL.Helpers.Mapping;
+using DAL.Entities;
+using DAL.Helpers;
+using Profile = AutoMapper.Profile;
+
+namespace BLL;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap(typeof(PagedList<>), typeof(PagedList<>))
+            .ConvertUsing(typeof(PagedListConverter<,>));
+        
+        CreateMap<ClothingItem, ClothingItemDto>().ReverseMap();
+        CreateMap<CreateClothingItemDto, ClothingItem>().ReverseMap();
+        CreateMap<ClothingItem, ClothingItemAllDto>()
+            .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.CategoryName))
+            .ForMember(dest => dest.TypeName, opt => opt.MapFrom(src => src.Type.TypeName))
+            .ForMember(dest => dest.TemperatureSuitabilityName, opt => opt.MapFrom(src => src.TemperatureSuitability.TemperatureSuitabilityName))
+            .ForMember(dest => dest.StyleNames, opt => opt.MapFrom(src => src.Styles.Select(s => s.Style.StyleName)))
+            .ForMember(dest => dest.SeasonNames, opt => opt.MapFrom(src => src.Seasons.Select(s => s.Season.SeasonName)));
+    }
+}
